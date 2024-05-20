f0efc0c7cc6da6f00c889bd4ed4773e43e26d489
Nước Mắm Code <nathan.ng.0808@gmail.com>
2024-05-19 23:37:51 -0500

    Update msg_server.py

--- msg_server.py
+++ msg_server.py
@@ -3,11 +3,14 @@ import socket
 import threading
 import json
 
+SERVER_NAME = ""
 HOST = 'localhost'
 PORT = 8000
 
 def _handle_connection(socket):
-    print('handle socket here')
+    print('Handling connection with: ', socket.getpeername())
+    data = socket.recv(1024)
+    print(data)
 
 def run_multithreaded():
     print('this is multithreaded')
@@ -15,17 +18,15 @@ def run_multithreaded():
 def run_singlethreaded():
     print('this is singlethreaded')
 
-
 #main
 if __name__=="__main__":
     if len(sys.argv) < 2:
-        print("Run 'python3 msg_server.py <port>'\nor 'python3 msg_server.py <port> -m'")
+        print('Run python3 msg_server.py <port number>\nor python3 msg_server.py <port number> -m')
         sys.exit(1)
         
     port = int(sys.argv[1])
-    multithreaded = len(sys.argv) > 2 and sys.argv[2] == '-m'
     
-    if multithreaded:
+    if (len(sys.argv) > 2 and sys.argv[2] == '-m'):
         #multithreaded
         run_multithreaded()
     else:
